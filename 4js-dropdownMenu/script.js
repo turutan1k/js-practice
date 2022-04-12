0b7a2a9a29360ed11e222b5c2ef662eaf0ab2680
turutan1k <turuta.nik1@gmail.com>
2022-04-12 18:01:55 +0300

    developed dropdown-menu

--- 4js-dropdownMenu/script.js
+++ 4js-dropdownMenu/script.js
@@ -1,24 +1,25 @@
-document.getElementById('nav').onmouseover = function(event){
-    let target = event.target;
-    if (target.className == 'menu-item') {
-        let s = target.getElementsByClassName('submenu');
-        closeMenu();
-        s[0].style.display='block';
-    }
-}
+document.getElementById("nav").onmouseover = function (event) {
+  let target = event.target;
+  if (target.className == "menu-item") {
+    let s = target.getElementsByClassName("submenu");
+    closeMenu();
+    s[0].style.display = "block";
+  }
+};
 
-document.onmouseover = function(event){
-    var target = event.target;
-    console.log(event.target);
-    if(target.className!='menu-item' && target.className!='submenu'){
-        closeMenu();
-    }
-}
+document.onmouseover = function (event) {
+  let target = event.target;
+  console.log(event.target);
+  if (target.className != "menu-item" && target.className != "submenu") {
+    closeMenu();
+    console.log(target);
+  }
+};
 
-function closeMenu(){
-    let menu = document.getElementById('nav');
-    let submenu = document.getElementsByClassName('submenu');
-    for (let i = 0; i < submenu.length; i++) {
-        submenu[i].style.display="none";
-    }
+function closeMenu() {
+  let menu = document.getElementById("nav");
+  let submenu = document.getElementsByClassName("submenu");
+  for (let i = 0; i < submenu.length; i++) {
+    submenu[i].style.display = "none";
+  }
 }
