9bbccb2432ecc2548445cdd8915769553a8df7f9
turutan1k <turuta.nik1@gmail.com>
2022-04-22 17:19:49 +0300

    revert commit 16js-convertMoney from 15js-accordion

--- 16js-convertMoney/script.js
+++
@@ -1,4 +0,0 @@
-function moneyConverter(valueNumber) {
-    document.getElementById('outputrub').innerHTML = valueNumber * 29.39;
-    document.getElementById('outputdol').innerHTML = valueNumber * 0.3;
-}
