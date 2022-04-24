75fb9cc090f562bb8eb900dfa95a8f6201764e3a
turutan1k <turuta.nik1@gmail.com>
2022-04-24 17:57:31 +0300

    17js-toDoApp

---
+++ 16js-convertMoney/script.js
@@ -0,0 +1,4 @@
+function moneyConverter(valueNumber) {
+    document.getElementById('outputrub').innerHTML = valueNumber * 29.39;
+    document.getElementById('outputdol').innerHTML = valueNumber * 0.3;
+}
